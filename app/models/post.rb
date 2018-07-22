62d53ee186f37c74a99442eb99f8258ead153360
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-22 17:35:39 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,18 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+  attr_accessor :name, :content
+
+  POSTS = []
+
+  def initialize(name, content)
+    @name = name
+    @content = content
+    POSTS << self
+  end
+
+  def self.all
+    POSTS
+  end
+  
 end
